60cbfa4f335c49e94d671591bd4038e98493541a
rodfalcon <rodfalcon2112@gmail.com>
2020-10-11 18:45:37 +0100

    Section 2 Done

---
+++ API/-- SQLite.sql
@@ -0,0 +1,12 @@
+-- SQLite
+INSERT INTO `Users` (Id, UserName)
+VALUES (1, "Rod");
+
+INSERT INTO `Users` (Id, UserName)
+VALUES (2, "Aoife");
+
+INSERT INTO `Users` (Id, UserName)
+VALUES (3, "Ernesto");
+
+INSERT INTO `Users` (Id, UserName)
+VALUES (4, "Jah");
