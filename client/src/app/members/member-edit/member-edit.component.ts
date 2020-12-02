f90865ae62f154b84e5d0fcd78533254eccdf3b4
rodfalcon <rodfalcon2112@gmail.com>
2020-12-02 03:21:14 +0000

    Terminando de arrumar

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -29,6 +29,7 @@ export class MemberEditComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadMember();
+    
   }
 
   loadMember() {
@@ -38,9 +39,10 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateMember() {
-    console.log(this.member);
-    this.toastr.success('Profile Updated Successfully');
-    this.editForm.reset(this.member);
+    this.memberService.updateMember(this.member).subscribe(() => {
+      this.toastr.success('Profile updated successfully');
+      this.editForm.reset(this.member);
+    })
   }
 
 }
